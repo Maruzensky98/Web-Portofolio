fd748277b08b40ab09b21d6e6378a491dfb82fa5
Anjar Rkz <ginanjarabdulhakim894@gmail.com>
2026-02-13 16:54:13 +0700

    script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        navLinks.classList.remove('active');
+    });
+});
+
+function showMessage() {
+alert("Terima kasih telah mengunjungi portofolio saya!");
+}
